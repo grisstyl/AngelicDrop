f03d3bc4d5ea49fe8a43953387935944cd4d875c
Tyler Grissom <minestein123@gmail.com>
2018-05-12 11:35:31 -0500

    Configuration-driven items added to controller

--- src/main/java/me/tylergrissom/angelicdrop/AngelicDropController.java
+++ src/main/java/me/tylergrissom/angelicdrop/AngelicDropController.java
@@ -4,11 +4,20 @@ import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import lombok.Getter;
 import me.tylergrissom.angelicdrop.config.MessagesYaml;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -72,5 +81,51 @@ public class AngelicDropController {
         } finally {
             logger.log(Level.INFO, "Reloaded plugin");
         }
+    }
+
+    public ItemStack createItem(ConfigurationSection section) {
+        Material material = Material.valueOf(section.getString("material").toUpperCase());
+        int amount = section.getInt("amount", 1);
+        byte data = (byte) section.getInt("data", 0);
+        short damage = (short) section.getInt("durability", 0);
+        boolean unbreakable = section.getBoolean("unbreakable", false);
+
+        ItemStack is = new ItemStack(material, amount, damage, data);
+        ItemMeta meta = is.getItemMeta();
+
+        meta.setUnbreakable(unbreakable);
+
+        if (section.get("display_name") != null) {
+            String name = ChatColor.translateAlternateColorCodes('&', section.getString("display_name"));
+
+            meta.setDisplayName(name);
+        }
+
+        if (section.get("lore") != null) {
+            List<String> originalLore = section.getStringList("lore");
+            List<String> lore = Collections.emptyList();
+
+            for (String str : originalLore) {
+                lore.add(ChatColor.translateAlternateColorCodes('&', str));
+            }
+
+            meta.setLore(lore);
+        }
+
+        meta.setUnbreakable(unbreakable);
+
+        is.setItemMeta(meta);
+
+        Map<Enchantment, Integer> enchantments = Collections.emptyMap();
+
+        section = section.getConfigurationSection("enchantments");
+
+        for (String key : section.getKeys(false)) {
+            enchantments.put(Enchantment.getByName(key.toUpperCase()), section.getInt(key, 1));
+        }
+
+        is.addUnsafeEnchantments(enchantments);
+
+        return is;
     }
 }
