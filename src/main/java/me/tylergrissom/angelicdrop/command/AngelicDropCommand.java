6cd79c82b888b16866950247be6b98bcddc80c46
Tyler Grissom <minestein123@gmail.com>
2018-05-18 18:26:53 -0600

    Added admin-facing permissions

--- src/main/java/me/tylergrissom/angelicdrop/command/AngelicDropCommand.java
+++ src/main/java/me/tylergrissom/angelicdrop/command/AngelicDropCommand.java
@@ -9,6 +9,7 @@ import me.tylergrissom.angelicdrop.config.MessagesYaml;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 
 /**
  * Copyright Tyler Grissom 2018
@@ -25,41 +26,49 @@ public class AngelicDropCommand extends CommandBase {
         MessagesYaml messages = controller.getMessages();
         String[] usage = messages.getMessages("command.usage");
 
-        if (sender.hasPermission("angelicdrop.admin")) {
-            if (args.length == 0) {
-                sender.sendMessage(usage);
-            } else {
-                String sub = args[0];
+        if (args.length == 0) {
+            sender.sendMessage(usage);
+        } else {
+            String sub = args[0];
 
-                if (sub.equalsIgnoreCase("start")) {
-                    if (sender instanceof Player) {
-                        Player p = (Player) sender;
-                        Selection selection = controller.getWorldEditIntegration().getSelection(p);
+            if (sub.equalsIgnoreCase("start")) {
+                if (!sender.hasPermission(new Permission("angelicdrop.start"))) {
+                    sender.sendMessage(messages.getMessage("error.no_permission"));
 
-                        if (selection != null) {
-                            controller.startDropParty(p, selection);
-                        } else {
-                            p.sendMessage(messages.getMessage("error.make_selection"));
-                        }
+                    return;
+                }
+
+                if (sender instanceof Player) {
+                    Player p = (Player) sender;
+                    Selection selection = controller.getWorldEditIntegration().getSelection(p);
+
+                    if (selection != null) {
+                        controller.startDropParty(p, selection);
                     } else {
-                        sender.sendMessage(messages.getMessage("error.only_players"));
-                    }
-                } else if (sub.equalsIgnoreCase("reload")) {
-                    try {
-                        sender.sendMessage(messages.getMessage("command.reloading"));
-
-                        controller.reloadPlugin();
-
-                        sender.sendMessage(messages.getMessage("command.reloaded"));
-                    } catch (Throwable ignored) {
-                        sender.sendMessage(messages.getMessage("error.reload_failed"));
+                        p.sendMessage(messages.getMessage("error.make_selection"));
                     }
                 } else {
-                    sender.sendMessage(usage);
+                    sender.sendMessage(messages.getMessage("error.only_players"));
                 }
+            } else if (sub.equalsIgnoreCase("reload")) {
+                if (!sender.hasPermission(new Permission("angelicdrop.reload"))) {
+                    sender.sendMessage(messages.getMessage("error.no_permission"));
+
+                    return;
+                }
+
+                try {
+                    sender.sendMessage(messages.getMessage("command.reloading"));
+
+                    controller.reloadPlugin();
+
+                    sender.sendMessage(messages.getMessage("command.reloaded"));
+                } catch (Throwable ignored) {
+                    sender.sendMessage(messages.getMessage("error.reload_failed"));
+                }
+            } else {
+                sender.sendMessage(usage);
             }
-        } else {
-            sender.sendMessage(messages.getMessage("error.no_permission"));
         }
     }
 }
